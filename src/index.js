c37f1f5cc4e59412d7479156f07fad1c949cba24
Petemc121 <petemc121@gmail.com>
2021-06-30 14:43:49 +0900

    reached the image section of the turorial. 'image' however is being called before initialized. Need to figure what's going on there

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Vietnam from './vietnam.jpg';
 
 function component() {
   const element = document.createElement('div');
+ 
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+   // Add the image to our existing div.
+   const Image = new Image();
+   Image.src = Vietnam;
+ 
+   element.appendChild(Vietnam);
 
   return element;
 }
